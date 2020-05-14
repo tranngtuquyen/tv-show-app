1acb0e03e301b88acf3d97dba59ce09fbd69a866
lavi09 <42918272+lavi09@users.noreply.github.com>
2020-05-14 14:38:05 -0700

    component.html and component.ts

--- src/app/tv-show/tv-show.component.ts
+++ src/app/tv-show/tv-show.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { IShow } from '../ishow';
+import { TvshowService } from '../tvshow.service';
 
 @Component({
   selector: 'app-tv-show',
@@ -6,10 +8,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tv-show.component.css']
 })
 export class TvShowComponent implements OnInit {
-
-  constructor() { }
+  show: IShow;
+  
+  constructor(private tvshowservice:TvshowService) { }
 
   ngOnInit(): void {
+    this.tvshowservice.getTvShow(2).subscribe((data) => (this.show = data));
+
   }
 
 }

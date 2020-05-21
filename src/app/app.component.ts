d2fa592d5d4705bf2648ec6f41fe8ebbbc94e4be
lavi09 <42918272+lavi09@users.noreply.github.com>
2020-05-21 13:25:52 -0700

    changes in search and tv show comp

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { IShow } from './ishow';
+import { TvshowService } from './tvshow.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'tv-show-app';
+  tvshow: IShow;
+
+  constructor(private tvshowService: TvshowService){}
+
+  doSearch(searchValue){
+    const userInput = searchValue;
+    this.tvshowService.getTvShowSearch(
+            userInput[0])
+            .subscribe(data => this.tvshow = data);
+  }
 }
--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-import { FormControl } from '@angular/forms';
+import { Component,Output,EventEmitter,OnInit } from '@angular/core';
+import { FormControl,Validators } from '@angular/forms';
 import { TvshowService } from '../tvshow.service';
+import { debounceTime } from 'rxjs/operators';
 
 @Component({
   selector: 'app-search',
@@ -8,19 +9,31 @@ import { TvshowService } from '../tvshow.service';
   styleUrls: ['./search.component.css']
 })
 export class SearchComponent implements OnInit {
-  search=new FormControl();
-  constructor(private tvshowService: TvshowService) { }
+  @Output() searchEvent = new EventEmitter<string>();
+  search=new FormControl('', [Validators.minLength(3)]);
+  constructor() { }
 
   ngOnInit(): void {
     this.search.valueChanges
-      .subscribe((searchValue: string) => {
+       .pipe(debounceTime(1000))
+       .subscribe((searchValue: string) => {
+      if (!this.search.invalid) {
+        this.searchEvent.emit(searchValue);
+      }
+    })
+}
+
+}
+
+
+      /*.subscribe((searchValue: string) => {
         if (searchValue){
           const userInput = searchValue;
           this.tvshowService.getTvShowSearch(userInput).subscribe(data => console.log(data));
             }
-      })
-  }
+      })*/
 
-  }
+
+
 
 
